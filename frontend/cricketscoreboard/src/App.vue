2a3ede19c9ccf5d3e40b530efdc10183ac3d23a2
alex <alex@pop-os.localdomain>
2023-09-28 03:27:44 -0500

    front end scoreboard functional

--- frontend/cricketscoreboard/src/App.vue
+++ frontend/cricketscoreboard/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import {ref} from 'vue'
 
 const numPlayers = 4
+const playerNames = ref(['Name','Name','Name','Name'])
 const scores = ref([])
 const totalScores = ref([])
 const scoreNumbers = ref([15,16,17,18,19,20,25])
+const scoreNames = ref([15,16,17,18,19,20,'B'])
+const buttonColors = ref(['white', 'lightgreen', 'orange', 'red'])
 
 for (let i = 0; i < numPlayers; i++) {
   scores.value.push(Array(scoreNumbers.value.length).fill(0))
@@ -18,6 +19,16 @@ function incrementScore(playerIndex, scoreIndex) {
     scores.value[playerIndex][scoreIndex]++
   } else {
     totalScores.value[playerIndex] += scoreNumbers.value[scoreIndex]
+  }
+  checkWin()
+}
+
+function checkWin() {
+  for (let i = 0; i < numPlayers; i++) {
+    const sum = scores.value[i].reduce((a,b) => a+b,0)
+    if (sum >= scores.value[i].length*3 && totalScores.value[i]==Math.max(...totalScores.value)) {
+      alert(playerNames.value[i] + ' wins!')
+    }
   }
 }
 
@@ -31,52 +42,84 @@ function incrementScore(playerIndex, scoreIndex) {
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Name</th>
-          <th>Name</th>
-          <th>Name</th>
-        </tr>
-        <tr v-for="(scoreNumber, scoreIndex) in scoreNumbers">
-          <td>{{ scoreNumber }}</td>
-          <td v-for="(player,playerIndex) in scores">
-            <button @click="incrementScore(playerIndex,scoreIndex)">{{ player[scoreIndex]}}</button>
+          <td></td>
+          <td>
+            <input v-model="playerNames[0]">
+          </td>
+          <td>
+            <input v-model="playerNames[1]">
+          </td>
+          <td>
+            <input v-model="playerNames[2]">
+          </td>
+          <td>
+            <input v-model="playerNames[3]">
           </td>
         </tr>
         <tr>
-          <td v-for="(total, index) in totalScores">
-            {{ total }}
-          </td>
+        <td class="scoreNum">Total Score</td>
+        <td class="scoreNum" v-for="(total, index) in totalScores">
+          {{ total }}
+        </td>
         </tr>
       </thead>
+      <tbody>
+        <tr v-for="(scoreNumber, scoreIndex) in scoreNumbers">
+          <td class="scoreNum">{{ scoreNames[scoreIndex] }}</td>
+          <td class="scoreRow" v-for="(player,playerIndex) in scores">
+            <button :style="{'background-color': buttonColors[player[scoreIndex]]}" @click="incrementScore(playerIndex,scoreIndex)">
+              {{ player[scoreIndex]}}
+            </button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+* {
+  box-sizing: content-box;
+  font-size: 3vh;
+}
+main {
+  width: 100%;
+  align-content: center;
+}
+table {
+  position: absolute;
+  table-layout: fixed;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-collapse: collapse;
+}
+tr {
+  height: 10%;
+  width: 100%
+}
+td {
+  height: 10%;
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.scoreRow{
+  height: 10%;
+  border: 1px solid #ccc;
+}
+.closed {
+  background-color: red;
+}
+.scoreNum {
+  height: 10%;
+  border: 1px solid #ccc;
+  font-weight: bold;
+
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  width: 95%;
+  height: 95%;
+  margin: 2px;
 }
 </style>
